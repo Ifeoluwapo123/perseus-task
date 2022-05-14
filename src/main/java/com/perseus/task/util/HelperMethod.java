71f0087237bec1c00f3257b6d38268349f223419
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2022-05-14 22:10:50 +0100

    helper methods

--- src/main/java/com/perseus/task/util/HelperMethod.java
+++ src/main/java/com/perseus/task/util/HelperMethod.java
@@ -1,5 +1,7 @@
 package com.perseus.task.util;
 
+import java.util.regex.Pattern;
+
 public final class HelperMethod {
     public static <E extends Enum<E>> E filterEnum(Class<E> clazz, String s) {
         for (var c : clazz.getEnumConstants()) {
@@ -7,5 +9,10 @@ public final class HelperMethod {
             if(each.equalsIgnoreCase(s)) return c;
         }
         return null;
+    }
+
+    public static boolean validateEmail(String email){
+        Pattern pattern = Pattern.compile("^.+@.+\\..+$");
+        return pattern.matcher(email).matches();
     }
 }
