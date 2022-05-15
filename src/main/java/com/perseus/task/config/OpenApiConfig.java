fc2bd52ad84896fae80a0cbdaf0d29e6339d7a84
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2022-05-15 07:43:51 +0100

    added open api documentation

---
+++ src/main/java/com/perseus/task/config/OpenApiConfig.java
@@ -0,0 +1,10 @@
+package com.perseus.task.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Perseus API", version = "v1"))
+public class OpenApiConfig {
+}
